fc13680756b840e996e3df6b035007183c896817
Niklas Baumstark <niklas.baumstark@gmail.com>
2013-10-17 15:57:17 +0200

    use glibtoolize on mac in snappy's autogen

--- get.sh
+++ get.sh
@@ -1,7 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
+set -e
 cd `dirname $0`
+rm -rf 3rd_party
 mkdir -p 3rd_party
 cd 3rd_party
+
 svn checkout http://snappy.googlecode.com/svn/trunk/ snappy
+pushd snappy
+patch < ../../snappy-autogen.patch
+popd
+
 git clone https://code.google.com/p/leveldb/
